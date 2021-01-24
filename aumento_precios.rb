f65bba60c03914d3d1925b5130a92894e1545161
evelyn gomez <evelyngomezfrez@gmail.com>
2021-01-24 17:33:19 -0300

    revisando y mejorando codigos

--- aumento_precios.rb
+++ aumento_precios.rb
@@ -1,11 +1,14 @@
-#Dentro del programa crear un método llamado augment que recibe un arreglo y 
-#un multiplicador, y que como resultado de un arreglo con todos los valores aumentados.
+=begin
+Dentro del programa crear un método llamado augment que recibe un arreglo y 
+un multiplicador, y que como resultado de un arreglo con todos los valores aumentados.
+=end
 
-def augment(array, factor)
-    new_array = []
-    array.each do |price|
-        new_array.push(price*factor)
+def augment(array, factor)#defino mi método que reciba un arreglo y un multiplicador
+    new_array = [] #este será mi arreglo que esta vacío
+    array.each do |price|#itero con each por que puedo hacer cambios al arreglo
+        new_array.push(price*factor)#con push guardo en mi nuevo arreglo y en paréntesis indico lo que se debe hacer precio por factor
     end
-    new_array
+    new_array #me devuelve
 end
-puts augment([10,20,10], 1.2)
+print augment([10,20,10], 1.2)
+print "\n"
